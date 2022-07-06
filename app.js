e4ee0830fd46a255d6f7866950f2970fd7d813c1
pengjiangyong <15366181328@189.cn>
2022-07-06 17:16:23 +0800

    1

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const api = require("./api/index");
 const { log } = require("./config/log4")
 //注入环境变量
 const dotenv = require("dotenv")
-dotenv.config({path:`./.env.${process.env.NODE_ENV}`})
+dotenv.config({ path: `./.env.${process.env.NODE_ENV}` })
 
 app.all('*', function (req, res, next) {
     //白名单
@@ -15,6 +15,9 @@ app.all('*', function (req, res, next) {
         res.header('Access-Control-Allow-Origin', req.headers.origin);
         res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length,Authorization, Accept, X-Requested-With,x-custom-header');
         res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    } else {
+        res.sendStatus(500);
+        return;
     }
 
     if (req.method == 'OPTIONS') {
