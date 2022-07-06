e56e4160747924484cb00ce50098ce7317f5d682
pengjiangyong <15366181328@189.cn>
2022-07-06 17:31:49 +0800

    1

--- app.js
+++ app.js
@@ -11,14 +11,11 @@ dotenv.config({ path: `./.env.${process.env.NODE_ENV}` })
 
 app.all('*', function (req, res, next) {
     //白名单
-    // if (originWhiteList.includes(req.headers.origin)) {
-    //     res.header('Access-Control-Allow-Origin', req.headers.origin);
-    //     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length,Authorization, Accept, X-Requested-With,x-custom-header');
-    //     res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-    // }
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length,Authorization, Accept, X-Requested-With,x-custom-header');
-    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    if (originWhiteList.includes(req.headers.origin)) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length,Authorization, Accept, X-Requested-With,x-custom-header');
+        res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    }
 
     if (req.method == 'OPTIONS') {
         res.sendStatus(200);
