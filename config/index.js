9a2eb8564c2276f9c88955853b976ba75bf42c73
pengjiangyong <15366181328@189.cn>
2022-07-19 09:59:21 +0800

    1

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
     //origin 白名单 仅针对指定地址允许跨域访问
-    originWhiteList: ['http://localhost:8082', 'http://42.192.11.55']
+    originWhiteList: ['http://localhost:8082', 'http://www.pjy.cool']
 }
