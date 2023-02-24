68180bb28ae323385a3857bf2dd9cd4cdd28b486
dingyongya <yahya_dyy@163.com>
2023-02-24 17:39:47 +0800

    openAi 接口添加

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
     //origin 白名单 仅针对指定地址允许跨域访问
-    originWhiteList: ['http://localhost:8082', 'http://www.pjy.cool','https://www.pjy.cool']
+    originWhiteList: ['http://localhost:8082', 'http://www.pjy.cool','https://www.pjy.cool', 'https://serve.pjy.cool/api/openAi/chat']
 }
