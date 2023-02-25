daa73ffbbf12f83047e61231505cbeebdf5702e0
丁永亚 <1085058441@qq.com>
2023-02-25 15:42:30 +0800

    在user 服务中调用openai

--- api/user.js
+++ api/user.js
@@ -480,4 +480,35 @@ router.post("/changePwd", tokenValidate, async (req, res) => {
         })
     }
 })
+// openAi
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
+
+router.post("/chat", async (req, res) => {
+    try {
+        const {title} = req.body
+        const {data} = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: title,
+            temperature: 0,
+            max_tokens: 2999,
+            top_p: 1,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: ["{}"],
+        })
+        console.log(data);
+        res.json({code: 200, data: data})
+    } catch (error) {
+        logError(error)
+        res.json({
+            code: 500,
+            msg: '出错啦',
+            data: null
+        })
+    }
+})
 module.exports = router
