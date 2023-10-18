f91f69ac90bfae3c16f9231dec503a2f16761648
pengjiangyong <15366181328@189.cn>
2023-10-18 16:05:12 +0800

    1

--- api/user.js
+++ api/user.js
@@ -5,32 +5,32 @@ const { pool, query } = require("../config/mysql.js")
 const { md5, createToken } = require("../util/index")
 const tokenValidate = require("../util/tokenValidate")
 const { logInput, logOutput, logError } = require("../config/log4")
-/**
- *
- * @api {post} /user/login 登录
- * @apiVersion 1.0.0
- * @apiName 登录
- * @apiGroup user
- * @apiBody {String} account 账号
- * @apiBody {String} passowrd 密码
- * @apiSuccess {Number} code 200
- * @apiSuccess {Object} data 用户信息
- * @apiSuccessExample {json} Success-Response:
- * HTTP/1.1 200 OK
- * {
- *  code: 200,
- *  data: {
- *    token: '',
- *    name:'',
- *    account:"",
- *    role:'',
- *    status:'',
- *
- *  }
- * }
- *
- */
-router.post("/login", async (req, res) => {
+    /**
+     *
+     * @api {post} /user/login 登录
+     * @apiVersion 1.0.0
+     * @apiName 登录
+     * @apiGroup user
+     * @apiBody {String} account 账号
+     * @apiBody {String} passowrd 密码
+     * @apiSuccess {Number} code 200
+     * @apiSuccess {Object} data 用户信息
+     * @apiSuccessExample {json} Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     *  code: 200,
+     *  data: {
+     *    token: '',
+     *    name:'',
+     *    account:"",
+     *    role:'',
+     *    status:'',
+     *
+     *  }
+     * }
+     *
+     */
+router.post("/login", async(req, res) => {
     const { account, password } = req.body;
 
     if (!account) {
@@ -85,7 +85,7 @@ router.post("/login", async (req, res) => {
         }
         if (user.password == md5(password)) {
             let token = createToken(user.id);
-            await query(`UPDATE user SET token=:token,tokenUpdateTime=:tokenUpdateTime,${pool.escapeId('limit')}=:limit WHERE id=:id`,
+            let data = await query(`UPDATE user SET token=:token,tokenUpdateTime=:tokenUpdateTime,${pool.escapeId('limit')}=:limit WHERE id=:id`,
 
                 { token, tokenUpdateTime: moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'), limit: 5, id: user.id }
             )
@@ -93,10 +93,10 @@ router.post("/login", async (req, res) => {
                 code: 200,
                 data: {
                     token: token,
-                    name: user.name,//用户名
-                    account: user.account,//账号
-                    role: user.role,//角色
-                    status: user.status,//账号状态：01 正常 02 锁定 03 禁用
+                    name: user.name, //用户名
+                    account: user.account, //账号
+                    role: user.role, //角色
+                    status: user.status, //账号状态：01 正常 02 锁定 03 禁用
 
                 },
                 msg: '登录成功，欢迎您！'
@@ -157,13 +157,13 @@ router.post("/login", async (req, res) => {
  * }
  *
  */
-router.get('/find', tokenValidate, async (req, res) => {
-    let { account, name, phone, pageIndex = 1, pageSize = 20 } = req.query;
+router.get('/find', tokenValidate, async(req, res) => {
+        let { account, name, phone, pageIndex = 1, pageSize = 20 } = req.query;
 
-    try {
-        pageIndex = Number(pageIndex)
-        pageSize = Number(pageSize)
-        let [user, [{ total }]] = await query(`
+        try {
+            pageIndex = Number(pageIndex)
+            pageSize = Number(pageSize)
+            let [user, [{ total }]] = await query(`
         SELECT 
         a.id, a.account, a.name, a.phone, a.remarks, a.status, a.createAt, 
         b.id AS roleId, b.name AS roleName 
@@ -174,276 +174,272 @@ router.get('/find', tokenValidate, async (req, res) => {
         ORDER BY createAt DESC
         LIMIT :limit OFFSET :offset;
         SELECT COUNT(*) as total FROM user;
-        `,
-            {
-                account, name, phone, limit: pageSize, offset: (pageIndex - 1) * pageSize
-            }
-        )
+        `, {
+                account,
+                name,
+                phone,
+                limit: pageSize,
+                offset: (pageIndex - 1) * pageSize
+            })
 
-        res.json({
-            code: 200,
-            data: user,
-            total,
-            msg: '查询成功'
-        })
-    } catch (error) {
-        logError(error)
-        res.json({
-            code: 500,
-            msg: '出错啦',
-            data: null
-        })
-    }
-})
-/**
- *
- * @api {post} /user/add 新增用户
- * @apiVersion 1.0.0
- * @apiName 新增用户
- * @apiGroup user
- * @apiBody {String} account 账号
- * @apiBody {String} password 密码
- * @apiBody {String} name 用户名称
- * @apiBody {String} phone 手机号
- * @apiBody {String} role 角色id
- * @apiBody {String} remarks 备注
- * @apiSuccess {Number} code 200
- * @apiSuccess {Object} data 用户信息
- * @apiSuccessExample {json} Success-Response:
- * HTTP/1.1 200 OK
- * {
- *  code: 200,
- *  data: null
- * }
- *
- */
-router.post("/add", tokenValidate, async (req, res) => {
-    const { account, password, name, phone, role, remarks } = req.body;
-    try {
-        let hasUser = await query(`
-            SELECT account FROM user WHERE account=:account
-        `,
-            { account })
-        if (hasUser.length > 0) {
+            res.json({
+                code: 200,
+                data: user,
+                total,
+                msg: '查询成功'
+            })
+        } catch (error) {
+            logError(error)
             res.json({
                 code: 500,
-                msg: '用户已存在',
+                msg: '出错啦',
                 data: null
             })
-            return;
         }
-        let data = await query(`
+    })
+    /**
+     *
+     * @api {post} /user/add 新增用户
+     * @apiVersion 1.0.0
+     * @apiName 新增用户
+     * @apiGroup user
+     * @apiBody {String} account 账号
+     * @apiBody {String} password 密码
+     * @apiBody {String} name 用户名称
+     * @apiBody {String} phone 手机号
+     * @apiBody {String} role 角色id
+     * @apiBody {String} remarks 备注
+     * @apiSuccess {Number} code 200
+     * @apiSuccess {Object} data 用户信息
+     * @apiSuccessExample {json} Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     *  code: 200,
+     *  data: null
+     * }
+     *
+     */
+router.post("/add", tokenValidate, async(req, res) => {
+        const { account, password, name, phone, role, remarks } = req.body;
+        try {
+            let hasUser = await query(`
+            SELECT account FROM user WHERE account=:account
+        `, { account })
+            if (hasUser.length > 0) {
+                res.json({
+                    code: 500,
+                    msg: '用户已存在',
+                    data: null
+                })
+                return;
+            }
+            let data = await query(`
         INSERT INTO user (account, password, name, phone, role, remarks,createAt,updateAt) VALUES (:account, :password, :name, :phone, :role, :remarks,:createAt,:updateAt)
-        `,
-            { account, password: md5(password), name, phone, role: Number(role), remarks, createAt: moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'), updateAt: moment(Date.now()).format('YYYY-MM-DD HH:mm:ss') }
-        )
-        if (data.affectedRows === 0) {
+        `, { account, password: md5(password), name, phone, role: Number(role), remarks, createAt: moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'), updateAt: moment(Date.now()).format('YYYY-MM-DD HH:mm:ss') })
+            if (data.affectedRows === 0) {
+                res.json({
+                    code: 500,
+                    msg: '添加失败',
+                    data
+                })
+                return;
+            }
             res.json({
-                code: 500,
-                msg: '添加失败',
+                code: 200,
+                msg: '添加成功',
                 data
             })
-            return;
+        } catch (error) {
+            logError(error)
+            res.json({
+                code: 500,
+                msg: '出错啦',
+                data: null
+            })
         }
-        res.json({
-            code: 200,
-            msg: '添加成功',
-            data
-        })
-    } catch (error) {
-        logError(error)
-        res.json({
-            code: 500,
-            msg: '出错啦',
-            data: null
-        })
-    }
-})
-/**
- *
- * @api {post} /user/update 修改用户
- * @apiVersion 1.0.0
- * @apiName 修改用户
- * @apiGroup user
- * @apiBody {String} id user id
- * @apiBody {String} account 账号
- * @apiBody {String} name 用户名称
- * @apiBody {String} phone 手机号
- * @apiBody {String} role 角色id
- * @apiBody {String} remarks 备注
- * @apiSuccess {Number} code 200
- * @apiSuccess {Object} data 用户信息
- * @apiSuccessExample {json} Success-Response:
- * HTTP/1.1 200 OK
- * {
- *  code: 200,
- *  data: null
- * }
- *
- */
-router.post('/update', tokenValidate, async (req, res) => {
-    const { id, account, name, phone, role, remarks } = req.body;
-    try {
-        let data = await query(`
+    })
+    /**
+     *
+     * @api {post} /user/update 修改用户
+     * @apiVersion 1.0.0
+     * @apiName 修改用户
+     * @apiGroup user
+     * @apiBody {String} id user id
+     * @apiBody {String} account 账号
+     * @apiBody {String} name 用户名称
+     * @apiBody {String} phone 手机号
+     * @apiBody {String} role 角色id
+     * @apiBody {String} remarks 备注
+     * @apiSuccess {Number} code 200
+     * @apiSuccess {Object} data 用户信息
+     * @apiSuccessExample {json} Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     *  code: 200,
+     *  data: null
+     * }
+     *
+     */
+router.post('/update', tokenValidate, async(req, res) => {
+        const { id, account, name, phone, role, remarks } = req.body;
+        try {
+            let data = await query(`
         UPDATE user SET account=:account, name=:name, phone=:phone, role=:role, remarks=:remarks
         WHERE id=:id
-        `,
-            { id, account, name, phone, role, remarks }
-        )
-        if (data.affectedRows === 0) {
+        `, { id, account, name, phone, role, remarks })
+            if (data.affectedRows === 0) {
+                res.json({
+                    code: 500,
+                    msg: '修改失败',
+                    data
+                })
+                return;
+            }
             res.json({
-                code: 500,
-                msg: '修改失败',
+                code: 200,
+                msg: '修改成功',
                 data
             })
-            return;
+        } catch (error) {
+            logError(error)
+            res.json({
+                code: 500,
+                msg: '出错啦',
+                data: null
+            })
         }
-        res.json({
-            code: 200,
-            msg: '修改成功',
-            data
-        })
-    } catch (error) {
-        logError(error)
-        res.json({
-            code: 500,
-            msg: '出错啦',
-            data: null
-        })
-    }
-})
-/**
- *
- * @api {post} /user/update 删除用户
- * @apiVersion 1.0.0
- * @apiName 删除用户
- * @apiGroup user
- * @apiBody {String} id user id
- * @apiSuccess {Number} code 200
- * @apiSuccess {Object} data 用户信息
- * @apiSuccessExample {json} Success-Response:
- * HTTP/1.1 200 OK
- * {
- *  code: 200,
- *  data: null
- * }
- *
- */
-router.post('/delete', tokenValidate, async (req, res) => {
-    let { id } = req.body;
-    try {
-        let data = await query(`
+    })
+    /**
+     *
+     * @api {post} /user/update 删除用户
+     * @apiVersion 1.0.0
+     * @apiName 删除用户
+     * @apiGroup user
+     * @apiBody {String} id user id
+     * @apiSuccess {Number} code 200
+     * @apiSuccess {Object} data 用户信息
+     * @apiSuccessExample {json} Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     *  code: 200,
+     *  data: null
+     * }
+     *
+     */
+router.post('/delete', tokenValidate, async(req, res) => {
+        let { id } = req.body;
+        try {
+            let data = await query(`
         DELETE FROM user WHERE id=:id
-        `,
-            { id })
-        if (data.affectedRows === 0) {
+        `, { id })
+            if (data.affectedRows === 0) {
+                res.json({
+                    code: 500,
+                    msg: '删除失败',
+                    data
+                })
+                return;
+            }
             res.json({
-                code: 500,
-                msg: '删除失败',
+                code: 200,
+                msg: '删除成功',
                 data
             })
-            return;
-        }
-        res.json({
-            code: 200,
-            msg: '删除成功',
-            data
-        })
-    } catch (error) {
-        logError(error)
-        res.json({
-            code: 500,
-            msg: '出错啦',
-            data: null
-        })
-    }
-})
-/**
- *
- * @api {post} /user/updateStatus 禁用 锁定、解锁
- * @apiVersion 1.0.0
- * @apiName 禁用 锁定、解锁
- * @apiGroup user
- * @apiBody {String} id user id
- * @apiBody {String} status user status
- * @apiSuccess {Number} code 200
- * @apiSuccess {Object} data 用户信息
- * @apiSuccessExample {json} Success-Response:
- * HTTP/1.1 200 OK
- * {
- *  code: 200,
- *  data: null
- * }
- *
- */
-router.post("/updateStatus", tokenValidate, async (req, res) => {
-    let { id, status } = req.body;
-    try {
-        if (!id) {
+        } catch (error) {
+            logError(error)
             res.json({
                 code: 500,
-                msg: 'id必填',
+                msg: '出错啦',
                 data: null
             })
-            return
         }
-        if (!['01', '02', '03'].includes(status)) {
-            res.json({
-                code: 500,
-                msg: 'status非法',
-                data: null
-            })
-            return;
+    })
+    /**
+     *
+     * @api {post} /user/updateStatus 禁用 锁定、解锁
+     * @apiVersion 1.0.0
+     * @apiName 禁用 锁定、解锁
+     * @apiGroup user
+     * @apiBody {String} id user id
+     * @apiBody {String} status user status
+     * @apiSuccess {Number} code 200
+     * @apiSuccess {Object} data 用户信息
+     * @apiSuccessExample {json} Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     *  code: 200,
+     *  data: null
+     * }
+     *
+     */
+router.post("/updateStatus", tokenValidate, async(req, res) => {
+        let { id, status } = req.body;
+        try {
+            if (!id) {
+                res.json({
+                    code: 500,
+                    msg: 'id必填',
+                    data: null
+                })
+                return
+            }
+            if (!['01', '02', '03'].includes(status)) {
+                res.json({
+                    code: 500,
+                    msg: 'status非法',
+                    data: null
+                })
+                return;
 
-        }
-        let data = await query(`
+            }
+            let data = await query(`
         UPDATE user set status=:status WHERE id=:id
-        `,
-            {
-                id, status
+        `, {
+                id,
+                status
             })
-        if (data.affectedRows === 0) {
+            if (data.affectedRows === 0) {
+                res.json({
+                    code: 500,
+                    msg: '操作失败',
+                    data
+                })
+                return;
+            }
             res.json({
-                code: 500,
-                msg: '操作失败',
+                code: 200,
+                msg: '操作成功',
                 data
             })
-            return;
-        }
-        res.json({
-            code: 200,
-            msg: '操作成功',
-            data
-        })
 
-    } catch (error) {
-        logError(error)
-        res.json({
-            code: 500,
-            msg: '出错啦',
-            data: null
-        })
-    }
-})
-/**
- *
- * @api {post} /user/changePwd 修改密码
- * @apiVersion 1.0.0
- * @apiName 修改密码
- * @apiGroup user
- * @apiBody {String} oldPwd 原密码
- * @apiBody {String} newPwd 新密码
- * @apiSuccess {Number} code 200
- * @apiSuccess {Object} data 用户信息
- * @apiSuccessExample {json} Success-Response:
- * HTTP/1.1 200 OK
- * {
- *  code: 200,
- *  data: null
- * }
- *
- */
-router.post("/changePwd", tokenValidate, async (req, res) => {
+        } catch (error) {
+            logError(error)
+            res.json({
+                code: 500,
+                msg: '出错啦',
+                data: null
+            })
+        }
+    })
+    /**
+     *
+     * @api {post} /user/changePwd 修改密码
+     * @apiVersion 1.0.0
+     * @apiName 修改密码
+     * @apiGroup user
+     * @apiBody {String} oldPwd 原密码
+     * @apiBody {String} newPwd 新密码
+     * @apiSuccess {Number} code 200
+     * @apiSuccess {Object} data 用户信息
+     * @apiSuccessExample {json} Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     *  code: 200,
+     *  data: null
+     * }
+     *
+     */
+router.post("/changePwd", tokenValidate, async(req, res) => {
     let { oldPwd, newPwd, userInfo } = req.body;
     try {
         if (md5(oldPwd) != userInfo.password) {
@@ -456,8 +452,7 @@ router.post("/changePwd", tokenValidate, async (req, res) => {
         }
         let data = await query(`
         UPDATE user SET password=:password WHERE id=:id
-        `,
-            { password: md5(newPwd), id: userInfo.id })
+        `, { password: md5(newPwd), id: userInfo.id })
         if (data.affectedRows === 0) {
             res.json({
                 code: 500,
