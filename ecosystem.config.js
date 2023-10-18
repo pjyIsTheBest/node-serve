482ac9a36f20bcc34644680cb7749ce13a43423a
pengjiangyong <15366181328@189.cn>
2023-10-18 18:11:17 +0800

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         name: "node-serve",
         script: "./app.js",
         env: {
-            NODE_ENV: "development",
+            NODE_ENV: "production",
         },
         env_production: {
             NODE_ENV: "production",
