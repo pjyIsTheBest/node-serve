ebce8f09b9bb15b98497b1f9f868fb5fd513ac4e
pengjiangyong <15366181328@189.cn>
2023-10-27 11:03:09 +0800

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
             repo: 'GIT_REPOSITORY',
             path: 'DESTINATION_PATH',
             'pre-deploy-local': '',
-            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js nodemon --env production',
             'pre-setup': ''
         }
     }
