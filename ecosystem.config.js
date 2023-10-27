b30d91be4df4d30fd656e5edbadf1bb2ceb705e0
pengjiangyong <15366181328@189.cn>
2023-10-27 11:17:19 +0800

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,17 +9,4 @@ module.exports = {
             NODE_ENV: "production",
         }
     }],
-
-    deploy: {
-        production: {
-            user: 'SSH_USERNAME',
-            host: 'SSH_HOSTMACHINE',
-            ref: 'origin/master',
-            repo: 'GIT_REPOSITORY',
-            path: 'DESTINATION_PATH',
-            'pre-deploy-local': '',
-            'post-deploy': 'npm install && pm2 reload ecosystem.config.js nodemon --env production',
-            'pre-setup': ''
-        }
-    }
 };
