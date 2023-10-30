29b69dae301a2741750e467351c3175b6ac9bd58
pengjiangyong <15366181328@189.cn>
2023-10-30 17:49:33 +0800

    1

--- util/wxapp.js
+++ util/wxapp.js
@@ -44,7 +44,7 @@ const getWxCode = async(page, scene) => {
             page,
             scene,
             check_path: true,
-            env_version: "trial"
+            env_version: "release"
         }
         let access_token = await getAccessToken();
         return new Promise((resolve, reject) => {
